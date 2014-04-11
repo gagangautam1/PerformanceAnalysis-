02bd80ae58ee09b6c5c6ecf3b11fc6f9ee71d804
gagangautam1 <gagan.gautam@sjsu.edu>
2014-04-10 19:39:40 -0700

    1st commit

---
+++ SimpleMathSolverClient/src/Services/Service.java
@@ -0,0 +1,13 @@
+/**
+ * Service.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package Services;
+
+public interface Service extends java.rmi.Remote {
+    public void find_prime() throws java.rmi.RemoteException;
+    public void check_prime() throws java.rmi.RemoteException;
+}
